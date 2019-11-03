43efe7906c51213f440bb1a437fb89891955031d
artibarti <safranyartur1995@gmail.com>
2019-11-03 12:35:41 +0100

    Updated sinus fit example

--- borjomi/examples/sinus_fit/sinus_fit.cpp
+++ borjomi/examples/sinus_fit/sinus_fit.cpp
@@ -8,12 +8,15 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <immintrin.h>
+#include <iomanip>
 #include <cstring>
 
 #include "borjomi/borjomi.h"
 
 using namespace borjomi;
+
+#define BATCH_SIZE 16
+#define NUMBER_OF_EPOCHS 2000
 
 void createTrainingData(matrix_t& x, matrix_t& sinx) {
 
@@ -24,46 +27,42 @@ void createTrainingData(matrix_t& x, matrix_t& sinx) {
   x.reshape(sampleCount, 1);
   sinx.reshape(sampleCount, 1);
 
-  unsigned index = 0;
-  for (float num = from; num < to; num += step) {
-    float vx = {num};
-    float vsinx = {sinf(num)};
-    x.at(index, 0) = vx;
-    sinx.at(index, 0) = vsinx;
-    index++;
+  for (float num = from, sampleIdx = 0; num <= to; num += step, sampleIdx++) {
+    x.at(sampleIdx, 0) = num;
+    sinx.at(sampleIdx, 0) = sinf(num);
   }
 }
 
 void train(Network& net, matrix_t& x, matrix_t& sinx) {
 
-  unsigned BATCH_SIZE = 16;
-  unsigned NUMBER_OF_EPOCHS = 2000;
   Adamax opt;
-  
-  int iEpoch = 0;
+  int epochIdx = 0;
+
   auto onEnumerateEpoch = [&]() {  
-    iEpoch++;
-    if (iEpoch % 100) return;
-    float loss = net.getLoss<LossFunctionType::Mse>(x, sinx);
-    std::cout << "epoch = " << iEpoch << "/" << NUMBER_OF_EPOCHS << " loss = " << loss << std::endl;
+    if (epochIdx%100 == 0 && epochIdx != 0) {
+      float loss = net.getLoss<LossFunctionType::Mse>(x, sinx);
+      std::cout << "epoch = " << epochIdx << "/"
+        << NUMBER_OF_EPOCHS << "\tloss = " << loss << std::endl;
+    }
+    epochIdx++;
   };
 
-  std::cout << "learning the sinus function with 2000 epochs:" << std::endl;
+  std::cout << std::endl << "Learning the sinus function with 2000 epochs..." << std::endl;
   net.fit<LossFunctionType::Mse>(opt, x, sinx, BATCH_SIZE, NUMBER_OF_EPOCHS, []() {}, onEnumerateEpoch);
 }
 
 void test(Network& net) {
 
-  std::cout << "Training finished, now computing prediction results:" << std::endl;
+  std::cout << std::endl << "Training finished, now computing prediction results:" << std::endl;
   float fMaxError = 0.f;
-  for (float x = -3.1416f; x < 3.1416f; x += 0.2f) {
+  for (float x = -3.1416; x < 3.1416; x += 0.2) {
 
-    matrix_t xv(1,1);
-    xv.at(0, 0) = x;
-    float fPredicted = net.predict(xv).at(0, 0);
+    matrix_t xv(1,1, x);
+    float fPredicted = net.predict(xv).at(0,0);
     float fDesired = sinf(x);
 
-    std::cout << "x = " << x << " sin(x) = " << fDesired << "   predicted = " << fPredicted << std::endl;
+    std::cout << "x = " << std::fixed << std::setprecision(5) << x << "\tsin(x) = " << fDesired
+      << "\tpredicted = " << fPredicted << std::endl;
 
     float fError = fabs(fPredicted - fDesired);
     if (fMaxError < fError) {
@@ -73,14 +72,14 @@ void test(Network& net) {
   std::cout << std::endl << "Max error = " << fMaxError << std::endl;
 }
 
-int main() {
+int main(int argc, char** argv) {
 
   Network net;
-  net << FullyConnectedLayer(1, 10, true, borjomi::engine_t::internal);
+  net << FullyConnectedLayer(1, 10, true, engine_t::internal);
   net << TanhLayer();
-  net << FullyConnectedLayer(10, 10, true, borjomi::engine_t::internal);
+  net << FullyConnectedLayer(10, 10, true, engine_t::internal);
   net << TanhLayer();
-  net << FullyConnectedLayer(10, 1, true, borjomi::engine_t::internal);
+  net << FullyConnectedLayer(10, 1, true, engine_t::internal);
 
   matrix_t x;
   matrix_t sinx;
